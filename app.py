f0d78b8e4ecb5593b6daaa01a124324b69f3f78d
michael-bocim <mtbocim@gmail.com>
2022-11-21 16:27:14 -0700

    Server receiving WS via socketio

--- app.py
+++ app.py
@@ -1,36 +1,64 @@
 from flask import Flask, request, render_template, jsonify, session
-import asyncio
-import websockets
+# import asyncio
+# import websockets
+
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 app.config["SECRET_KEY"] = "this-is-secret"
 
-async def handler(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-        except websockets.ConnectionClosedOK:
-            print("connection closed")
-            break
-        print(message)
-        await websocket.send(f"{message} message recieved")
-
-#coroutine that manages a connection (handler)
-#listener for where the server can be reached
-#
-async def main():
-    async with websockets.serve(handler, "", 8001):
-        print("connection established")
-        await asyncio.Future()  # run forever
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.get("/")
-def homepage():
-    """Shows the homepage"""
-    asyncio.run(main())
+@socketio.on('connect')
+def test_connect():
+    emit('after connect',  {'data':'Lets dance'})
+
+@socketio.on('message')
+def receive_msg(message):
+    print(message)
+
+@socketio.on('test')
+def receive_test(test):
+    print(test)
+
+# @socketio.on("score")
 
 
-    return render_template("index.html")
+if __name__ == '__main__':
+    socketio.run(app)
+
+# async def handler(websocket):
+#     while True:
+#         try:
+#             message = await websocket.recv()
+#         except websockets.ConnectionClosedOK:
+#             print("connection closed")
+#             break
+#         print(message)
+#         await websocket.send(f"{message} message recieved")
+
+# #coroutine that manages a connection (handler)
+# #listener for where the server can be reached
+# #
+# async def main():
+#     async with websockets.serve(handler, "", 8001):
+#         print("connection established")
+#         await asyncio.Future()  # run forever
+#         print("after future")
+
+
+# # if __name__ == "__main__":
+# #     asyncio.run(main())
+
+# @app.get("/")
+# def homepage():
+#     """Shows the homepage"""
+#     asyncio.run(main())
+
+
+#     return render_template("index.html")
--- static/index.js
+++ static/index.js
@@ -1,6 +1,10 @@
 "use strict";
+var socket = io();
+        socket.on('connect', function() {
+            socket.emit('my event', {data: 'I\'m connected!'});
+        });
 
-const websocket = new WebSocket("ws://localhost:8001/");
+//const websocket = new WebSocket("ws://localhost:8001/");
 
 const $BUTTON = $("#test-form-button");
 const $FORM_INPUT = $("#text-to-send");
@@ -9,11 +13,13 @@ const $INCOMING = $("#recieved");
 $BUTTON.on("click", sendMessage);
 function sendMessage(evt){
     evt.preventDefault();
-    websocket.send($FORM_INPUT.val());
+    // websocket.send($FORM_INPUT.val());
+    socket.emit("test", "some other data here");
+    socket.emit("message",$FORM_INPUT.val());
 }
 
-websocket.addEventListener("message", recieveMessage)
-
+//websocket.addEventListener("message", recieveMessage)
+socket.addEventListener("message", recieveMessage)
 function recieveMessage(data){
     console.log(data);
     $INCOMING.append(`<p>${data.data}</p>`)
